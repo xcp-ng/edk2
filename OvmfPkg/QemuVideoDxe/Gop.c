87fd8eb59d2d68bc94c9b08dfc736e60eacaa8dc
Vates Git Importer <gitimporter@vates.tech>
2015-09-19 07:09:42 +0100

    Add support for NVIDIA vGPU
    When NVIDIA vGPU is used, demu emulates something very similar to QEMU's
    stdvga. Tweak the code to support it.

--- OvmfPkg/QemuVideoDxe/Gop.c
+++ OvmfPkg/QemuVideoDxe/Gop.c
@@ -66,7 +66,12 @@ QemuVideoCompleteModeData (
                     (VOID **)&FrameBufDesc
                     );
 
-  Mode->FrameBufferBase = FrameBufDesc->AddrRangeMin;
+  if (Private->Variant == QEMU_VIDEO_BOCHS_NVIDIA)
+    Mode->FrameBufferBase = ((EFI_PHYSICAL_ADDRESS)BochsRead (Private,
+                                 VBE_DISPI_INDEX_LFB_ADDRESS_H)) << 16 |
+                             BochsRead (Private, VBE_DISPI_INDEX_LFB_ADDRESS_L);
+  else
+    Mode->FrameBufferBase = FrameBufDesc->AddrRangeMin;
   Mode->FrameBufferSize = Info->HorizontalResolution * Info->VerticalResolution;
   Mode->FrameBufferSize = Mode->FrameBufferSize * ((ModeData->ColorDepth + 7) / 8);
   Mode->FrameBufferSize = EFI_PAGES_TO_SIZE (
@@ -183,6 +188,7 @@ Routine Description:
       InitializeCirrusGraphicsMode (Private, &QemuVideoCirrusModes[ModeData->InternalModeIndex]);
       break;
     case QEMU_VIDEO_BOCHS_MMIO:
+    case QEMU_VIDEO_BOCHS_NVIDIA:
     case QEMU_VIDEO_BOCHS:
       InitializeBochsGraphicsMode (Private, ModeData);
       break;
