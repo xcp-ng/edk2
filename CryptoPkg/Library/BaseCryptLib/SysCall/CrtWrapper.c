053ec2b87538fab48abf6528fa1e634dd4f6499b
Gerd Hoffmann <kraxel@redhat.com>
2023-08-03 12:37:26 +0800

    [PATCH 18/18] CryptoPkg/BaseCryptLib: drop BIO_* dummy functions
    openssl 3.0 requires a functional BIO_sprintf() implementation.
    
    Signed-off-by: Gerd Hoffmann <kraxel@redhat.com>
    Cc: Jiewen Yao <jiewen.yao@intel.com>
    Cc: Xiaoyu Lu <xiaoyu1.lu@intel.com>
    Cc: Guomin Jiang <guomin.jiang@intel.com>
    Reviewed-by: Jiewen Yao <jiewen.yao@intel.com>
    Acked-by: Ard Biesheuvel <ardb@kernel.org>
    Tested-by: Ard Biesheuvel <ardb@kernel.org>
    Tested-by: Brian J. Johnson <brian.johnson@hpe.com>
    Tested-by: Kenneth Lautner <klautner@microsoft.com>
    (cherry picked from commit 2a6dc1211f9b73ca6264ce7c2ab1a8dfeb1f1c0f)

--- CryptoPkg/Library/BaseCryptLib/SysCall/CrtWrapper.c
+++ CryptoPkg/Library/BaseCryptLib/SysCall/CrtWrapper.c
@@ -472,31 +472,6 @@ fwrite (
   return 0;
 }
 
-//
-//  -- Dummy OpenSSL Support Routines --
-//
-
-int
-BIO_printf (
-  void        *bio,
-  const char  *format,
-  ...
-  )
-{
-  return 0;
-}
-
-int
-BIO_snprintf (
-  char        *buf,
-  size_t      n,
-  const char  *format,
-  ...
-  )
-{
-  return 0;
-}
-
 #ifdef __GNUC__
 
 typedef
