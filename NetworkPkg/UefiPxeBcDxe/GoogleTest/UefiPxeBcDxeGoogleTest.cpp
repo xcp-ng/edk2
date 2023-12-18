68c51af27e7de05a794c5a6696da6e6f0377f2c9
Doug Flick <dougflick@microsoft.com>
2023-12-18 11:17:25 -0800

    SECURITY PATCH TCBZ4539 - CVE-2023-45234 - Host Based Unit Test

---
+++ NetworkPkg/UefiPxeBcDxe/GoogleTest/UefiPxeBcDxeGoogleTest.cpp
@@ -0,0 +1,21 @@
+/** @file
+  Acts as the main entry point for the tests for the UefiPxeBcDxe module.
+  Copyright (c) Microsoft Corporation
+  SPDX-License-Identifier: BSD-2-Clause-Patent
+**/
+#include <gtest/gtest.h>
+
+////////////////////////////////////////////////////////////////////////////////
+// Add test files here
+// Google Test will only pick up the tests from the files that are included
+// here.
+////////////////////////////////////////////////////////////////////////////////
+#include "PxeBcDhcp6GoogleTest.cpp"
+
+////////////////////////////////////////////////////////////////////////////////
+// Run the tests
+////////////////////////////////////////////////////////////////////////////////
+int main(int argc, char* argv[]) {
+  testing::InitGoogleTest (&argc, argv);
+  return RUN_ALL_TESTS ();
+}
